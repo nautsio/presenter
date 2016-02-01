f86d23d47fa751b7f83f1870f73345c1e6135845
Erik Veld <eveld@xebia.com>
2016-02-01 17:18:29 +0100

    Polish & update version

--- commands.go
+++ commands.go
@@ -65,37 +65,42 @@ func doInit(c *cli.Context) {
 func doServe(c *cli.Context) {
 	master := c.Bool("master")
 
+	// Get the presentation path from the command line, or grab the current directory.
 	presentationPath, _ := os.Getwd()
 	if len(c.Args()) > 0 {
 		presentationPath = c.Args()[0]
 	}
 
+	// Check if the path is relative.
 	if !strings.HasPrefix(presentationPath, "/") {
 		presentationPath, _ = filepath.Abs(presentationPath)
 	}
 
+	// Check if there is a presentation file present.
 	if _, err := os.Stat(path.Join(presentationPath, "/slides.md")); err != nil {
 		fmt.Printf("slides.md does not exist at %s\n", presentationPath)
 		os.Exit(1)
 	}
 
-	// Handle the slides
+	// Handle the slides.
 	http.HandleFunc("/slides.md", func(w http.ResponseWriter, r *http.Request) {
 		t, _ := template.ParseFiles(path.Join(presentationPath, "slides.md"))
 		t.Execute(w, "")
 	})
 
-	// Handle the images
+	// Handle images.
 	http.HandleFunc("/img/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, path.Join(presentationPath, r.URL.Path))
 	})
 
+	// Handle reveal.js files.
 	http.Handle("/css/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, "reveal.js"}))
 	http.Handle("/js/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, "reveal.js"}))
 	http.Handle("/lib/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, "reveal.js"}))
 	http.Handle("/plugin/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, "reveal.js"}))
 	http.Handle("/theme/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, ""}))
 
+	// Handle the website.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		opt := &Option{Markdown: "slides.md", Master: master}
 		indexHTML, _ := Asset("theme/index.html")
